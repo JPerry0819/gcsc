18507d9cfa7ed9286bbe2d977c029ba7711d88d5
Jessica Bruner <jessicabruner23@outlook.com>
2024-01-10 22:50:22 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+   <nav className="navbar">
+    <p className="title-bar">Graham County Substance Coalition</p>
+    <button className="nav-button">About Us</button>
+    <button className="nav-button">Recovery Angels</button>
+    <button className="nav-button">12 Step Meetings</button>
+    <button className="nav-button">Re-entry Program</button>
+    <button className="nav-button">Housing</button>
+    <button className="nav-button">Events/Donations</button>
+
+      </nav>
+
+      <section className="section-about"></section>
+    </div>
+  );
+}
+
+export default App;
